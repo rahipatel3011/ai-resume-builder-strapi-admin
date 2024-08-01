1b532b30a09d0cb55d70789517e1760047a4684b
Rahi <rahipatel.30@gmail.com>
2024-08-01 18:33:58 -0400

    Initial Realease 1.0

---
+++ types/generated/components.d.ts
@@ -0,0 +1,328 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface SkillsSkillsFilename extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'brush';
+  };
+  attributes: {
+    skillType: Schema.Attribute.String;
+    skillValues: Schema.Attribute.JSON;
+  };
+}
+
+export interface SkillsSkillsFilename extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'brush';
+  };
+  attributes: {
+    skillType: Schema.Attribute.String;
+    skillValues: Schema.Attribute.JSON;
+  };
+}
+
+export interface SkillsSkillsFilename extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'brush';
+  };
+  attributes: {
+    skillType: Schema.Attribute.String;
+    skillValues: Schema.Attribute.JSON;
+  };
+}
+
+export interface SkillsSkillsFilename extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'brush';
+  };
+  attributes: {
+    skillType: Schema.Attribute.String;
+    skillValues: Schema.Attribute.JSON;
+  };
+}
+
+export interface SkillsSkillsFilename extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'brush';
+  };
+  attributes: {
+    skillType: Schema.Attribute.String;
+    skillValues: Schema.Attribute.JSON;
+  };
+}
+
+export interface ProjectProjectFilename extends Struct.ComponentSchema {
+  collectionName: 'components_project_projects';
+  info: {
+    displayName: 'Project';
+    icon: 'briefcase';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    projectSummary: Schema.Attribute.Text;
+    technologies: Schema.Attribute.String;
+  };
+}
+
+export interface ProjectProjectFilename extends Struct.ComponentSchema {
+  collectionName: 'components_project_projects';
+  info: {
+    displayName: 'Project';
+    icon: 'briefcase';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    projectSummary: Schema.Attribute.Text;
+    technologies: Schema.Attribute.String;
+  };
+}
+
+export interface ProjectProjectFilename extends Struct.ComponentSchema {
+  collectionName: 'components_project_projects';
+  info: {
+    displayName: 'Project';
+    icon: 'briefcase';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    projectSummary: Schema.Attribute.Text;
+    technologies: Schema.Attribute.String;
+  };
+}
+
+export interface ProjectProjectFilename extends Struct.ComponentSchema {
+  collectionName: 'components_project_projects';
+  info: {
+    displayName: 'Project';
+    icon: 'briefcase';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    projectSummary: Schema.Attribute.Text;
+    technologies: Schema.Attribute.String;
+  };
+}
+
+export interface ProjectProjectFilename extends Struct.ComponentSchema {
+  collectionName: 'components_project_projects';
+  info: {
+    displayName: 'Project';
+    icon: 'briefcase';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    projectSummary: Schema.Attribute.Text;
+    technologies: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperienceFilename extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    icon: 'cup';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    currentlyWorking: Schema.Attribute.Boolean;
+  };
+}
+
+export interface ExperienceExperienceFilename extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    icon: 'cup';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    currentlyWorking: Schema.Attribute.Boolean;
+  };
+}
+
+export interface ExperienceExperienceFilename extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    icon: 'cup';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    currentlyWorking: Schema.Attribute.Boolean;
+  };
+}
+
+export interface ExperienceExperienceFilename extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    icon: 'cup';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    currentlyWorking: Schema.Attribute.Boolean;
+  };
+}
+
+export interface ExperienceExperienceFilename extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'experience';
+    icon: 'cup';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    currentlyWorking: Schema.Attribute.Boolean;
+  };
+}
+
+export interface EducationEducationFilename extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'earth';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    description: Schema.Attribute.Text;
+    currentlyStudying: Schema.Attribute.Boolean;
+  };
+}
+
+export interface EducationEducationFilename extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'earth';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    description: Schema.Attribute.Text;
+    currentlyStudying: Schema.Attribute.Boolean;
+  };
+}
+
+export interface EducationEducationFilename extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'earth';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    description: Schema.Attribute.Text;
+    currentlyStudying: Schema.Attribute.Boolean;
+  };
+}
+
+export interface EducationEducationFilename extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'earth';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    description: Schema.Attribute.Text;
+    currentlyStudying: Schema.Attribute.Boolean;
+  };
+}
+
+export interface EducationEducationFilename extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'earth';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.Date;
+    endDate: Schema.Attribute.Date;
+    description: Schema.Attribute.Text;
+    currentlyStudying: Schema.Attribute.Boolean;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'skills\\skills.__filename__': SkillsSkillsFilename;
+      'skills\\skills.__filename__': SkillsSkillsFilename;
+      'skills\\skills.__filename__': SkillsSkillsFilename;
+      'skills\\skills.__filename__': SkillsSkillsFilename;
+      'skills\\skills.__filename__': SkillsSkillsFilename;
+      'project\\project.__filename__': ProjectProjectFilename;
+      'project\\project.__filename__': ProjectProjectFilename;
+      'project\\project.__filename__': ProjectProjectFilename;
+      'project\\project.__filename__': ProjectProjectFilename;
+      'project\\project.__filename__': ProjectProjectFilename;
+      'experience\\experience.__filename__': ExperienceExperienceFilename;
+      'experience\\experience.__filename__': ExperienceExperienceFilename;
+      'experience\\experience.__filename__': ExperienceExperienceFilename;
+      'experience\\experience.__filename__': ExperienceExperienceFilename;
+      'experience\\experience.__filename__': ExperienceExperienceFilename;
+      'education\\education.__filename__': EducationEducationFilename;
+      'education\\education.__filename__': EducationEducationFilename;
+      'education\\education.__filename__': EducationEducationFilename;
+      'education\\education.__filename__': EducationEducationFilename;
+      'education\\education.__filename__': EducationEducationFilename;
+    }
+  }
+}
